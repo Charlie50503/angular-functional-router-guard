b264b5bb85dc2ec02dfb629ac72e529f08ae0153
DESKTOP-0HRQPN4\営業所 <cate50503@gmail.com>
2023-04-29 19:14:59 +0800

    方法一

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, inject } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { LoginService } from './login.service';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"page1",
+  component:Page1Component,
+  canActivate:[()=>{
+    return inject(LoginService).isLegginged ? true : false
+  }]
+},{
+  path:"page2",
+  component:Page2Component
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-functional-router-guard';
+
+  loginService = inject(LoginService)
+  toggleLogin(){
+    this.loginService.isLegginged = !this.loginService.isLegginged
+  }
 }
